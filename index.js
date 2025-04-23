4bf63e8b2e712449017682ee30135bc2a5d7c029
P.R. Neider-Ball <phillrballn@icloud.com>
2025-04-23 07:50:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,19 @@
 //    }, 1000);
 //}
 
-function hoistMe() {
-    console.log(a)
-    var a = 10
-    console.log(a)
+//function hoistMe() {
+//    console.log(a)
+//    var a = 10
+//    console.log(a)
+//}
+
+//hoistMe();
+
+
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log("var loop:", i), 1000);
 }
 
-hoistMe();
+for (let j = 0; j < 3; j++) {
+    setTimeout(() => console.log("let loop:", j), 1000);
+}
